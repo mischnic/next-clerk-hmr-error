8233308e5dc7c069d0e5b73e273f39a67fb3e62b
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-10-07 16:06:00 +0200

    f

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,4 +1,4 @@
-import * as Sentry from "@sentry/nextjs";
+// import * as Sentry from "@sentry/nextjs";
 
 // const {
 //   HttpInstrumentation,
@@ -19,31 +19,31 @@ import * as Sentry from "@sentry/nextjs";
 // }
 
 export async function register() {
-  // if (process.env.NEXT_RUNTIME === "nodejs") {
-  // await import("./sentry.server.config");
-  // Sentry.init({
-  //   dsn: process.env.SENTRY_DSN,
-  //   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  //   tracesSampleRate: 1,
-  //   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  //   debug: true,
-  // });
-  // try {
-  // new X();
-  const { Hook } = require("require-in-the-middle");
-  new Hook(
-    // Intercept all `require` calls; we will filter the matching ones below
-    null,
-    { internals: true },
-    (exports: any, name: any, basedir: any) => {
-      return exports;
-    }
-  );
-  // new HttpInstrumentation();
-  // } catch (e) {
-  //   console.log(e);
-  // }
-  // }
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    // await import("./sentry.server.config");
+    // Sentry.init({
+    //   dsn: process.env.SENTRY_DSN,
+    //   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    //   tracesSampleRate: 1,
+    //   // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    //   debug: true,
+    // });
+    // try {
+    // new X();
+    const { Hook } = require("require-in-the-middle");
+    new Hook(
+      // Intercept all `require` calls; we will filter the matching ones below
+      null,
+      { internals: true },
+      (exports: any, name: any, basedir: any) => {
+        return exports;
+      }
+    );
+    // new HttpInstrumentation();
+    // } catch (e) {
+    //   console.log(e);
+    // }
+  }
 
   // if (process.env.NEXT_RUNTIME === "edge") {
   //   await import("./sentry.edge.config");
