c3b281c16dbb9c7d7e676ea7c9d0b620567395d1
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-10-07 15:05:03 +0200

    f

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
-import {
-  ClerkProvider /* SignedIn, SignedOut, SignInButton, UserButton */,
-} from "@clerk/nextjs";
+// import {
+//   ClerkProvider /* SignedIn, SignedOut, SignInButton, UserButton */,
+// } from "@clerk/nextjs";
+// import { ClerkProvider } from "../lib";
+// import { ClerkProvider } from "@clerk/nextjs/dist/esm/app-router/server/ClerkProvider.js";
+// import { ClientClerkProvider } from "@clerk/nextjs/dist/esm/app-router/client/ClerkProvider.js";
+import { ClientClerkProvider } from "../lib/ClientClerkProvider.js";
+
+const ClerkProvider = ClientClerkProvider;
 
 // console.log( process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY );
 
