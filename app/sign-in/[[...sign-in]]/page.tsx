8e1220077427e11fde93060660ab54540119ae2d
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-10-07 13:28:53 +0200

    f

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,21 @@
+"use client";
+
 import { SignIn } from "@clerk/nextjs";
 
-import TriggerButton from "./TriggerBug";
+import { triggerBug } from "./trigger-bug";
+
+// import TriggerButton from "./TriggerBug";
+function TriggerButton() {
+  return (
+    <button
+      onClick={() => triggerBug()}
+      className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+    >
+      OR CLICK THIS TO TRIGGER THE BUG
+    </button>
+  );
+}
+
 export default function SignInPage(): React.JSX.Element {
   return (
     <div>
